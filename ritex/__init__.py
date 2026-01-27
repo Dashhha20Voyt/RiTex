0b3fa708372ed57384e073a2957f9fa2aba35355
Darya Voitenko <134844764+Dashhha20Voyt@users.noreply.github.com>
2026-01-27 11:24:21 +0300

    Create __init__.py

---
+++ ritex/__init__.py
@@ -0,0 +1,10 @@
+# ritex/__init__.py
+"""
+RiTex: A Python package for batch effect correction using Riemannian geometry.
+"""
+
+from .core.transformer import RiTexTransformer
+
+__version__ = "0.1.0"
+__author__ = "Darya Voitenko"
+__all__ = ["RiTex"]
